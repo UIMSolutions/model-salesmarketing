ebb3f829d7b1d84d0cb44c19fdf87a8b972be5dd
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-31 07:16:07 +0200

    Update technicalname.d

--- source/models/salesmarketing/entities/hmim/technicalname.d
+++ source/models/salesmarketing/entities/hmim/technicalname.d
@@ -1,2 +1,34 @@
 module models.salesmarketing.entities.hmim.technicalname
 ;
+module models.salesmarketing.entities.agreementexternalagreementclassificationcod;
+
+@safe:
+import models.salesmarketing;
+
+class DLogisticsLocationRoleEntity : DOOPEntity {
+  mixin(EntityThis!("LogisticsLocationRoleEntity"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([ // individual values
+purpose		GAB/LogisticsLocationRoleEntity
+postalAddress		GAB/LogisticsLocationRoleEntity
+contactInfo		GAB/LogisticsLocationRoleEntity
+language		GAB/LogisticsLocationRoleEntity
+type		GAB/LogisticsLocationRoleEntity
+backingTable_LogisticsLocationRoleTranslationRelationshipId		GAB/LogisticsLocationRoleEntity
+      ])
+      .registerPath("addressbooks_logistics.locationroles");
+  }
+}
+mixin(EntityCalls!("LogisticsLocationRoleEntity"));
+
+version(test_library) {
+  unittest {
+    assert(LogisticsLocationRoleEntity);
+  
+    auto entity = LogisticsLocationRoleEntity;
+  }
+}
