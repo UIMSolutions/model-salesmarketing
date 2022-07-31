fe5b9f3ef73cb3b23f0054ee90935f20bd7a2b07
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-31 06:51:37 +0200

    Update commissionsalesrepresentative.d

--- source/models/salesmarketing/entities/commissionsalesrepresentative.d
+++ source/models/salesmarketing/entities/commissionsalesrepresentative.d
@@ -2,3 +2,31 @@ module models.salesmarketing.entities.commissionsalesrepresentative;
 
 @safe:
 import models.salesmarketing;
+
+class DLogisticsLocationRoleEntity : DOOPEntity {
+  mixin(EntityThis!("LogisticsLocationRoleEntity"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([ // individual values
+purpose		GAB/LogisticsLocationRoleEntity
+postalAddress		GAB/LogisticsLocationRoleEntity
+contactInfo		GAB/LogisticsLocationRoleEntity
+language		GAB/LogisticsLocationRoleEntity
+type		GAB/LogisticsLocationRoleEntity
+backingTable_LogisticsLocationRoleTranslationRelationshipId		GAB/LogisticsLocationRoleEntity
+      ])
+      .registerPath("addressbooks_logistics.locationroles");
+  }
+}
+mixin(EntityCalls!("LogisticsLocationRoleEntity"));
+
+version(test_library) {
+  unittest {
+    assert(LogisticsLocationRoleEntity);
+  
+    auto entity = LogisticsLocationRoleEntity;
+  }
+}
