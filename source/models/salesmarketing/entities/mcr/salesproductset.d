1ccb48ff80c5f086a0e3cc96715ea6fc298d5005
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-09 06:50:51 +0200

    Adding attributes to MCRSalesProductSetEntity

--- source/models/salesmarketing/entities/mcr/salesproductset.d
+++ source/models/salesmarketing/entities/mcr/salesproductset.d
@@ -1,33 +1,31 @@
-
-module models.salesmarketing.entities.agreementexternalagreementclassificationcod;
+module models.salesmarketing.entities.mcr.salesproductset;
 
 @safe:
 import models.salesmarketing;
 
-class DLogisticsLocationRoleEntity : DOOPEntity {
-  mixin(EntityThis!("LogisticsLocationRoleEntity"));
+class DMCRSalesProductSetEntity : DOOPEntity {
+  mixin(EntityThis!("MCRSalesProductSetEntity"));
   
   override void initialize() {
     super.initialize;
 
     this
       .addValues([ // individual values
-purpose		GAB/LogisticsLocationRoleEntity
-postalAddress		GAB/LogisticsLocationRoleEntity
-contactInfo		GAB/LogisticsLocationRoleEntity
-language		GAB/LogisticsLocationRoleEntity
-type		GAB/LogisticsLocationRoleEntity
-backingTable_LogisticsLocationRoleTranslationRelationshipId		GAB/LogisticsLocationRoleEntity
+        "productSetNumber": StringAttribute,
+        "productSetDescription": StringAttribute,
+        "customerAccountNumber": StringAttribute,
+        "backingTable_MCRItemListTableRelationshipId": StringAttribute,
+        "relationship_PrimaryCompanyContextRelationshipId": StringAttribute,
       ])
-      .registerPath("salesmarketing_logistics.locationroles");
+      .registerPath("salesmarketing_mcr.salesproductsets");
   }
 }
-mixin(EntityCalls!("LogisticsLocationRoleEntity"));
+mixin(EntityCalls!("MCRSalesProductSetEntity"));
 
 version(test_modul_salesmarketing) {
   unittest {
-    assert(LogisticsLocationRoleEntity);
+    assert(MCRSalesProductSetEntity);
   
-    auto entity = LogisticsLocationRoleEntity;
+    auto entity = MCRSalesProductSetEntity;
   }
 }
