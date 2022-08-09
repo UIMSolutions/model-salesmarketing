8d4b0cc973cfe24ea009ce0c277c19730f2ae265
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-08-09 06:33:22 +0200

    updates

--- source/models/salesmarketing/entities/tam/funds/trade/tradeallowanceagreementmerchandisingeventlumpsum.d
+++ source/models/salesmarketing/entities/tam/funds/trade/tradeallowanceagreementmerchandisingeventlumpsum.d
@@ -1,2 +1,34 @@
 module models.salesmarketing.entities.tam.tradeallowanceagreementmerchandisingeventlumpsum;
 
+module models.salesmarketing.entities.agreementexternalagreementclassificationcod;
+
+@safe:
+import models.salesmarketing;
+
+class DLogisticsLocationRoleEntity : DOOPEntity {
+  mixin(EntityThis!("LogisticsLocationRoleEntity"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([ // individual values
+purpose		GAB/LogisticsLocationRoleEntity
+postalAddress		GAB/LogisticsLocationRoleEntity
+contactInfo		GAB/LogisticsLocationRoleEntity
+language		GAB/LogisticsLocationRoleEntity
+type		GAB/LogisticsLocationRoleEntity
+backingTable_LogisticsLocationRoleTranslationRelationshipId		GAB/LogisticsLocationRoleEntity
+      ])
+      .registerPath("salesmarketing_logistics.locationroles");
+  }
+}
+mixin(EntityCalls!("LogisticsLocationRoleEntity"));
+
+version(test_modul_salesmarketing) {
+  unittest {
+    assert(LogisticsLocationRoleEntity);
+  
+    auto entity = LogisticsLocationRoleEntity;
+  }
+}
