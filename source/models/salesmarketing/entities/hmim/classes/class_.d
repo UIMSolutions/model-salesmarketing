163dc303a55526b1cd8f638804cce1bbbffc5b09
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-02-20 16:15:15 +0100

    Enhance call initialize (DConfigurationValue)

--- source/models/salesmarketing/entities/hmim/classes/class_.d
+++ source/models/salesmarketing/entities/hmim/classes/class_.d
@@ -6,7 +6,7 @@ import models.salesmarketing;
 class DHMIMClassEntity : DOOPEntity {
   mixin(EntityThis!("HMIMClassEntity"));
   
-  override void initialize() {
+  override void initialize(DConfigurationValue configSettings = null) {
     super.initialize(configSettings);
 
     this
