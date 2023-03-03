47e2ef2b26064bf708562e4987ebab81d9685e30
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-03 16:06:11 +0100

    bugfixing

--- source/models/salesmarketing/package.d
+++ source/models/salesmarketing/package.d
@@ -1,1 +1,14 @@
 module models.salesmarketing;
+
+public import std.stdio;
+public import std.string;
+public import std.uuid;
+
+// Dub
+public import vibe.d;
+
+public import uim.core;
+public import uim.oop;
+
+public import models.salesmarketing.classes;
+public import models.salesmarketing.entities;
