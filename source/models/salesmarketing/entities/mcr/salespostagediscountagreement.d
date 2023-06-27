fa952af7506a99092592e57ae359d0bd54d0d7f1
UIMSolutions <ui.manufaktur@gmail.com>
2023-06-27 08:48:06 +0200

    Rename OOPEntity in Entity

--- source/models/salesmarketing/entities/mcr/salespostagediscountagreement.d
+++ source/models/salesmarketing/entities/mcr/salespostagediscountagreement.d
@@ -4,7 +4,7 @@ module models.salesmarketing.entities.mcr.salespostagediscountagreement;
 @safe:
 import models.salesmarketing;
 
-class DMCRSalesPostageDiscountAgreementEntity : DOOPEntity {
+class DMCRSalesPostageDiscountAgreementEntity : DEntity {
   mixin(EntityThis!("MCRSalesPostageDiscountAgreementEntity"));
   
   override void initialize(DConfigurationValue configSettings = null) {
